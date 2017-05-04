5ecf5a199d78d54b6698039909de041337f4dcb6
Adrian Gracia <Shyscope@gmail.com>
2017-05-04 10:43:21 -0500

    BPM is more accurate

--- Lab11.ino
+++ Lab11.ino
@@ -3,45 +3,66 @@
 #define pinInt A8
 int times[5];
 int ctr, last, ref, rate;
-float timeref;
+float timeref,timeend;
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
 
-  
+  int i;
+  for(i=22;i<30; i++){ //set up pins 22-29
+    pinMode(i, OUTPUT);
+  }
   pinMode(pinInt, INPUT_PULLUP);
   enableInterrupt(pinInt, isr, RISING);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:x
-  /*
-  if(ctr%4==0){
-    float summationOfTimes = 0;
-    for(int i=0; i < 5;i++)
-      summationOfTimes += times[i];
+  delay(5000);
+  int count=ctr;
+  if(count >= 5){
+    int i,j;
+    ctr=0;
+    timeend=millis();
+    Serial.print("count: ");
+    Serial.print(count);
+    Serial.print("\ntime lapse: ");
+    Serial.println((timeend-timeref));
+    float bpm = ( (count/(timeend-timeref)) * 60000);
+    Serial.print("bpm: ");
+    Serial.println(bpm);
 
-    //Serial.print("BPM: ");  
-    //Serial.println((ctr/summationOfTimes)*60000);
-    //delay(3000);
-  }*/
-  Serial.println(ctr);
+    int tens, ones;
+    //For tens
+    tens = ((int)bpm)/10;
+    //For ones
+    ones = ((int)bpm)%10;
+
+    for(j = 0; j < 4; j++){
+      if(bitRead(tens,j) == 1){
+        digitalWrite(j+22,HIGH);
+      }
+      else{
+         digitalWrite(j+22,LOW);
+      }
+    }
+
+    for(j = 0; j < 4; j++){
+      if(bitRead(ones,j) == 1){
+        digitalWrite(j+26,HIGH);
+      }
+      else{
+         digitalWrite(j+26,LOW);
+      }
+    }
+    
+  }
 }
 
 
 void isr(void){    
+  //ctr++;
+  if(ctr==0)
+    timeref=millis();
   ctr++;
-/*
- // time = millis();
- // if(ctr == 0)
- //   timeref = time;
-  Serial.print("interrupt ");
-  Serial.println(ctr);
-  times[ctr] = millis();
-  last = ctr;
-  ctr++;
-  if(ctr==5) ctr = 0;
-  */
 }
 
